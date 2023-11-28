5998f53f6580249216b717a82505ddd7825774a0
yaasiinaxmed <yaskassoy@gmail.com>
2023-11-28 15:19:21 +0300

    update

--- api/routers/user.router.js
+++ api/routers/user.router.js
@@ -1,5 +1,8 @@
 import express from 'express'
+import { GoogleAuth } from '../controllers/user.controller.js'
 
 const router = express.Router()
+
+router.post("/googleAuth", GoogleAuth)
 
 export default router
