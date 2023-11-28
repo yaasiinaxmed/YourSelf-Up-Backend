978ec9c9d11c95f8b03c19290c833b21f426061f
yaasiinaxmed <yaskassoy@gmail.com>
2023-11-28 13:45:36 +0300

    created userRouter

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import userRouter from './routers/user.router.js'
 
 dotenv.config()
 
@@ -15,5 +16,7 @@ mongoose.connect(process.env.DATABASE_URL).then(() => {
 }).catch((error) => {
     console.log('Connecting MongoDB Error:', error)
 })
+
+server.use("/api/user", userRouter)
 
 export default server
