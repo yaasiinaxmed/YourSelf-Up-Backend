99d72b76d24914949c09944230b1f166b58dda0e
yaasiinaxmed <yaskassoy@gmail.com>
2023-11-28 13:31:54 +0300

    setup

---
+++ api/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const server = express()
+
+server.use(express.json())
+server.use(cors())
+
+mongoose.connect(process.env.DATABASE_URL).then(() => {
+    console.log("Connected MongoDB")
+}).catch((error) => {
+    console.log('Connected MongoDB Error:', error)
+})
+
+export default server
